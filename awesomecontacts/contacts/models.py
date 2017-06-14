472348317c2a47354813b5e05974c9e782bee69e
szpone <nikola.adamus@gmail.com>
2017-06-14 11:12:40 +0200

    create contacts model

--- awesomecontacts/contacts/models.py
+++ awesomecontacts/contacts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    last_name = models.CharField(max_length=64, null=False)
+    phone_number = models.IntegerField()
+    job = models.CharField(max_length=64, null=False)
+    company = models.CharField(max_length=64, null=False)
+    email = models.EmailField(null=False)
