3f1c39a33a2431c3407e2a4bcd947f4e9a12f02e
szpone <nikola.adamus@gmail.com>
2017-06-19 09:41:39 +0200

    save form

--- awesomecontacts/contacts/views.py
+++ awesomecontacts/contacts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 # from django.views.generic.edit import FormView
 from contacts.forms import ContactForm
 from django.views import View
+from contacts.models import Contact
 
 # Create your views here.
 
@@ -18,12 +19,14 @@ class ContactFormView(View):
     def post(self, request):
         form = ContactForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            last_name = form.cleaned_data['last_name']
-            phone_number = form.cleaned_data['phone_number']
-            job = form.cleaned_data['job']
-            company = form.cleaned_data['company']
-            email = form.cleaned_data['email']
+            obj = Contact()
+            obj.name = form.cleaned_data['name']
+            obj.last_name = form.cleaned_data['last_name']
+            obj.phone_number = form.cleaned_data['phone_number']
+            obj.job = form.cleaned_data['job']
+            obj.company = form.cleaned_data['company']
+            obj.email = form.cleaned_data['email']
+            obj.save()
             return HttpResponse("Thx!")
 
 
