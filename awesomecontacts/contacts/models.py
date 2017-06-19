17b552b1910ca7759b5c38712b5ddcf61d47f982
szpone <nikola.adamus@gmail.com>
2017-06-19 11:26:49 +0200

    change order of name and last name records

--- awesomecontacts/contacts/models.py
+++ awesomecontacts/contacts/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
-    name = models.CharField(max_length=64, null=False)
     last_name = models.CharField(max_length=64, null=False)
+    name = models.CharField(max_length=64, null=False)
     phone_number = models.IntegerField()
     job = models.CharField(max_length=64, null=False)
     company = models.CharField(max_length=64, null=False)
     email = models.EmailField(null=False)
 
     def __str__(self):
-        return '%s %s %s %s %s %s' % (self.name, self.last_name,
+        return '%s %s %s %s %s %s' % (self.last_name, self.name,
                                 self.phone_number, self.job,
                                 self.company, self.email)
